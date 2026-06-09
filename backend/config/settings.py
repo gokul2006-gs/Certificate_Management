f3424f7eb4c140f8ce1f3b91a1f896af4b5cde6e
GokulS <gokulsat73@gmail.com>
2026-06-09 14:09:36 +0530

    DB changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -201,3 +201,19 @@ STORAGES = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path(
+        '',
+        lambda request: JsonResponse({"status": "ok", "service": "smart-certificate-api"}),
+    ),
 
     path(
         'admin/',
