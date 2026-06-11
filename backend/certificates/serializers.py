132bc9755495772022fbc7705a3e48f66881e5b2
GokulS <gokulsat73@gmail.com>
2026-06-11 16:15:08 +0530

    Qr fix

--- backend/certificates/serializers.py
+++ backend/certificates/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from accounts.models import Student
 from .models import Certificate
 
 class CertificateSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    student = serializers.PrimaryKeyRelatedField(
+        queryset=Student.objects.all(),
+        pk_field=serializers.CharField(),
+        required=False,
+    )
     student_id = serializers.CharField(source="student.student_id", read_only=True)
     student_name = serializers.CharField(source="student.name", read_only=True)
     course_name = serializers.CharField(source="student.course.course_name", read_only=True)
